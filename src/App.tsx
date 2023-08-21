0c231096b3ba47fc8ef93dfbaac8edb41180d5aa
Yehor Kononchuk <yehorkononchuk14@gmail.com>
2023-08-21 17:06:14 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora totam inventore excepturi repudiandae! Animi, vero? Officiis illo ipsa ipsum magni?
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
